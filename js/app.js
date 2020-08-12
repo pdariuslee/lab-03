15aa8f67b2cb93d92190fc399497b0c81ca8ac6a
Darius Lee Pasilaban <p.dariuslee@gmail.com>
2020-08-11 20:28:35 -0700

    updated html, css and js files

--- js/app.js
+++ js/app.js
@@ -1,37 +1,71 @@
-'use strict'
+'use strict';
 
-// const images = [];
-
-// const Image (name, src, alt){
-//   this.name = name;
-//   this.src = src;
-//   this.alt = alt;
-
-//   Image.images.push(this);
-// }
-
-// Image.prototype.displayImages = function() {
-
-// }
-
-// =======================
-
+// constructor function
 const animalArray = [];
-function Animal (name, src, hobbies){
-  this.name = name;
-  this.src = src;
-  this.hobbies = hobbies;
+
+function Animal (title, image_url, description, keyword, horns){
+  this.name = title;
+  this.src = image_url;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+
   animalArray.push(this);
 }
+
+Animal.prototype.renderImage = function() {
+
+  const $clonedSectionEl = $('#photo-template').clone();
+  // console.log($clonedSectionEl);
+
+  // $clonedSectionEl.find('h2').text(this.name);
+  $clonedSectionEl.find('img').attr('src', this.src).attr('alt', this.description);
+  // $clonedSectionEl.find('p').text(this.description);
+
+  $('main').append($clonedSectionEl);
+
+};
+
+Animal.prototype.filterImages = function(){
+
+  const $clonedOptionEl = $('#dropdown').clone();
+
+  $clonedOptionEl.text(this.keyword);
+  $('select').append($clonedOptionEl);
+
+
+
+};
+
+//Animal.populateFilter = function()
+//empty array
+//loop pover animal array
+// if no keyword, push into array
+// if empty array has keyword, then skip
+// loop over new array w/ keywords
+//string of option tag w/ keyword -- appended to
+// needs sep optiont tags for dropdown to function
+
+
+
+
 const animalsFromData = hornedAnimals => {
   hornedAnimals.forEach( horns => {
-    new Animal (horns.name, horns.image_url, horns.hobbies);
+    new Animal (horns.title, horns.image_url, horns.description, horns.keyword, horns.horns);
   });
-  //  eventually iterate through `animalArray` &
-  //  render w/ Jquery
+
+  animalArray.forEach(animalValue => animalValue.renderImage());
+
+  animalArray.forEach(animalValue => animalValue.filterImages());
+
 };
 const pullObject = {
-  method: ‘get’,
-  dataType: ‘json’
+  method: 'get',
+  dataType: 'json'
 };
-$.ajax(‘data/page-1.json’).then(animalsFromData);
+$.ajax('data/page-1.json', pullObject).then(animalsFromData);
+
+
+
+console.log(animalArray);
+
